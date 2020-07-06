22c53db15833229dc728851e29a97921a6e3e08f
Bertha <fbertha121@gmail.com>
2020-07-06 11:01:02 -0500

    Agregando Ejercicio4 de imprimir valores de todas las variables

---
+++ Ejercicios/Ejercicio4 - Imprimir valores de todas las variables/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int main(int argc, char** argv) {
+	bool ValorBoolean;
+	ValorBoolean=true;
+	
+	int ValorEntero=15;
+	double ValorDouble=20.99;
+	string ValorString="Hola como estan";
+	char ValorChar='B';
+	
+	cout<<"Valor Boolean:" << ValorBoolean << endl;
+	cout<<"Valor Entero:" << ValorEntero << endl;
+	cout<<"Valor Double:" << ValorDouble << endl;
+	cout<<"Valor String:" << ValorString << endl;
+	cout<<"Valor Char:" << ValorChar << endl;
+
+
+	return 0;
+}
