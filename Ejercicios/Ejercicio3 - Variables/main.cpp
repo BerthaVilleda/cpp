36478f96ba734d91d14dcf1c24ea776397736b84
Bertha <fbertha121@gmail.com>
2020-07-06 10:33:03 -0500

    Agregando ejercicio3 de variables

---
+++ Ejercicios/Ejercicio3 - Variables/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int main(int argc, char** argv) {
+	int NumeroEntero;
+	NumeroEntero=15;
+	
+	cout<<"Entero:" << NumeroEntero << endl;
+	
+	NumeroEntero=18;
+	
+	cout<<"Entero Modificado:" << NumeroEntero;
+	
+	return 0;
+}
