2c2ed17d0b50bfb5235cc29b33bb03c3d0fc539a
Bertha <fbertha121@gmail.com>
2020-07-17 15:54:25 -0500

    Agregando Ejercicio25 Suma de pares e impares y total

---
+++ Ejercicios/Ejercicio25 - Suma de pares e imapres y total/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+int main(int argc, char** argv) {
+
+int pares = 0;
+int total = 0;
+int impares = 0;
+
+for (int i = 1; i <= 10; i++)
+{
+    cout << i << " ";
+    
+    if (i % 2 == 0)
+{
+    pares += i;
+}
+else
+
+    impares += i;
+
+    total = pares + impares;
+
+}
+cout << endl;
+cout << "\nTotal pares:" << pares;
+cout << endl;
+cout << "\nTotal impares:" << impares;
+cout << endl;
+cout << "\nTotal:" << total;
+
+return 0;
+}
