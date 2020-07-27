f76defaf0f80e979e60978343c48f28fd683ab26
Bertha <fbertha121@gmail.com>
2020-07-27 16:34:42 -0500

    Agregando Ejercicio37 Repaso III

---
+++ Ejercicios/Ejercicio37 - Repaso III/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+int main(int argc, char** argv) {
+	
+	int a = 0;
+	int b = 10;
+	int c = 4;
+	
+	//if (a > b && a > c){
+	//	cout << "a es mayor que b o mayor que c";
+	//}
+	
+	//while(a > b){
+	//	a--;
+		
+	//	if (a == 20){
+	//		break;
+	//	}
+	//}
+	
+	while(true)
+	{
+		while(true)
+		{
+			if (a == 10)
+			{
+				break;
+			}
+			b++;
+		}
+		if(a == 1)
+		{
+			break;
+		}
+		 a++;
+	}
+	
+	return 0;
+}
