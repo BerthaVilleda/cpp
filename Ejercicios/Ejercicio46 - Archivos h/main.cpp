d6e061908dab64c194bd81790cb7babfb8ebff55
Bertha <fbertha121@gmail.com>
2020-08-03 16:25:32 -0500

    Agregando Ejercicio46 Archivos h

---
+++ Ejercicios/Ejercicio46 - Archivos h/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "calculadora.h"
+#include "alumnos.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	cout << calcular(5, 7, '+');
+	cout << endl;
+	cout << obtenerNombreCompleto();
+	cout << endl;
+	cout << obtenerEdad();
+	
+	return 0;
+}
