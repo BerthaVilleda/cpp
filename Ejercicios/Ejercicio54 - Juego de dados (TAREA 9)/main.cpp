0035d630a4b2e5ca0f0124256a5080fbfb6da1ec
Bertha <fbertha121@gmail.com>
2020-08-05 17:46:36 -0500

    Agregando Ejercicio54 (TAREA 9)

---
+++ Ejercicios/Ejercicio54 - Juego de dados (TAREA 9)/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int dado1 = 0;
+	int dado2 = 0;
+	int suma = 0;
+	
+	srand (time(NULL));
+	
+	dado1 = rand() % 6 + 1;
+	dado2 = rand() % 6 + 1;
+	
+	cout << "Dado 1:" << dado1;
+	
+	cout << endl;
+	
+	cout << "Dado 2:" << dado2;
+	
+	suma = dado1 + dado2;
+	
+	return 0;
+}
