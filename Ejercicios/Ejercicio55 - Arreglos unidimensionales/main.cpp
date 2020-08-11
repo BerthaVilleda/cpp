00f9b338c4f1be39208aebdc850204ad704f3f36
Bertha <fbertha121@gmail.com>
2020-08-11 14:22:56 -0500

    Agregando Ejercicio55 Arreglos unidimensionales

---
+++ Ejercicios/Ejercicio55 - Arreglos unidimensionales/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int numeros[5];
+	
+	numeros[0] = 10;
+	numeros[1] = 5;
+	numeros[2] = 7;
+	numeros[3] = 50;
+	numeros[4] = 2;
+	
+	cout << numeros[0] << endl;
+	cout << numeros[1] << endl;
+	cout << numeros[2] << endl;
+	cout << numeros[3] << endl;
+	cout << numeros[4] << endl;
+	
+	return 0;
+}
